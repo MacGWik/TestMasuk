d69a3364551b0f6843387d44f0ce971bd399ffb9
M Argus Chopin Gyver <arguspolim@gmail.com>
2019-02-11 16:01:18 +0700

    test ke dua

--- app/Admin.php
+++ app/Admin.php
@@ -10,4 +10,5 @@ class Admin extends Model
     protected $table = 'admin';
     protected $primaryKey = 'id_admin';
     // testing untuk nambahin comment, sekalian latihan git add push dll
+    // ini test lagi
 }
